82ef89d388fe8eecd64a7a47831cc867425a45c7
EmersonPaduan <emerson.paduan@gmail.com>
2024-04-15 21:51:11 -0300

    aula07 - ex02

--- aula07/exercicio01/Exercicio02.java
+++ aula07/exercicio01/Exercicio02.java
@@ -3,48 +3,50 @@ package exercicio01;
 import java.util.Random;
 import java.util.Scanner;
 
+/*
+ * Jogo - Craps
+ */
+
 public class Exercicio02 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        String enter;
         int dado1, dado2, soma;
         Random random = new Random();
-        boolean naoTerminou = true;
 
-        while (naoTerminou) {
-            System.out.println("tecle ENTER para jogar os dados:");
-            entrada.nextLine();
-            dado1 = random.nextInt(6) + 1;
-            dado2 = random.nextInt(6) + 1;
-            System.out.printf("Dados: %d e %d\n", dado1, dado2);
-            soma = dado1 + dado2;
+        System.out.print("tecle ENTER para jogar os dados:");
+        entrada.nextLine();
+        dado1 = random.nextInt(6) + 1;
+        dado2 = random.nextInt(6) + 1;
+        System.out.printf("Dados: %d e %d\n", dado1, dado2);
+        soma = dado1 + dado2;
 
-            if (soma == 7 || soma == 11) {
-                System.out.println("Você ganhou!");
-                naoTerminou = false;
+        if (soma == 7 || soma == 11) {
+            System.out.println("Você ganhou!");
+        } else {
+            if (soma == 2 || soma == 3 || soma == 12) {
+                System.out.println("Você perdeu!");
             } else {
-                if (soma == 2 || soma == 3 || soma == 12) {
-                    System.out.println("Você perdeu!");
-                    naoTerminou = false;
-                } else {
-                    int objetivo = soma;
-                    while(naoTerminou) {
-                        System.out.println("tecle ENTER para jogar os dados:");
-                        entrada.nextLine();
-                        dado1 = random.nextInt(6) + 1;
-                        dado2 = random.nextInt(6) + 1;
-                        soma = dado1 + dado2;
-                        if(soma == objetivo) {
-                            System.out.printf("Você ganhou: %d e %d\n", dado1, dado2);
+                int objetivo = soma;
+                boolean naoTerminou = true;
+                System.out.println("tecle ENTER para jogar os dados. Objetivo: " + objetivo);
+                while (naoTerminou) {
+                    entrada.nextLine();
+                    dado1 = random.nextInt(6) + 1;
+                    dado2 = random.nextInt(6) + 1;
+                    System.out.printf("Dados: %d e %d", dado1, dado2);
+                    soma = dado1 + dado2;
+                    if (soma == objetivo) {
+                        System.out.println("Você ganhou!");
+                        naoTerminou = false;
+                    } else {
+                        if (soma == 7) {
+                            System.out.println("Você perdeu!");
                             naoTerminou = false;
-                        } else {
-
                         }
                     }
                 }
             }
         }
-
         entrada.close();
     }
 }
